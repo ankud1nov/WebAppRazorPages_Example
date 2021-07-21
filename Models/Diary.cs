9da1a0876d7f21b4d91e6b5908082d8dc3cdbd2f
Ilya Ankudinov <Ilya.Ankudinov@russianpost.ru>
2021-07-21 16:25:47 +0400

    Добавьте файлы проекта.

---
+++ Models/Diary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Models
+{
+    public class Diary
+    {
+        public int ID { get; set; }
+        [ForeignKey("DairyType")]
+        public int DiaryTypeId { get; set; }
+        public string Subject { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime DateTimeBegin { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime DateTimeEnd { get; set; }
+        public string Place { get; set; }
+    }    
+}
---
+++ Models/DiaryType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class DiaryType
+    {
+        public int ID { get; set; }
+        public string Type { get; set; }
+    }
+}
